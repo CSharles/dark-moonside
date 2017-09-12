5d26b175767e2e591fc2b80010ee6675e2f2c489
Carlos Garcia <Csharles@users.noreply.github.com>
2017-09-12 12:57:13 -0600

    Updated Reposirory to fech from Postgres

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -6,50 +6,37 @@
     require __DIR__ ."/../Entity/User.php";
 class UserRepository
 {
-    private $database;
+    protected $pdo;
 
     public function __construct() 
     {
-        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/darkmoonside-15dec-firebase-adminsdk-iu306-174f379fd0.json');
-        $firebase = (new Factory)
-            ->withServiceAccount($serviceAccount)
-            ->create();
-        $this->database = $firebase->getDatabase();
+        $dbopts = parse_url(getenv('DATABASE_URL'));
+        $this->pdo=new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo',
+        array(
+        'pdo.server' => array(
+            'driver'   => 'pgsql',
+            'user' => $dbopts["user"],
+            'password' => $dbopts["pass"],
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'dbname' => ltrim($dbopts["path"],'/')
+            )
+        ));
     }
     
     public function showUsers()
     {
-     $data =$this->database->getReference('user');
-     $users = $data->orderByChild('email');
-     return $users;
+        $statement = $this->pdo->prepare('SELECT * FROM users');
+        $statement->execute();
+        $game = $statement->fetchObject('User');
+        return $game;
     }
 
     public function retriveUser($name, $password)
     {
         try {
-            $ch = curl_init();
-        
-            if (FALSE === $ch)
-                throw new Exception('failed to initialize');
-                
-            curl_setopt($ch, CURLOPT_URL, 'https://darkmoonside-15dec.firebaseio.com/user.json?pasword?&email='.$name.'&password='.$password.'');
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);  
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_HEADER, false); 
-        
-            $content = curl_exec($ch);
-        
-            if (FALSE === $content)
-                throw new Exception(curl_error($ch), curl_errno($ch));
-        
-                $value=$content;
+       
         } catch(Exception $e) {
-        
-            trigger_error(sprintf(
-                'Curl failed with error #%d: %s',
-                $e->getCode(), $e->getMessage()),
-                E_USER_ERROR);
         
         }
         return $value;
