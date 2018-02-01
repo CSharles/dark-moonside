cdcf5151ee643c4e90785800d595211b60757976
Carlos Garcia <Csharles@users.noreply.github.com>
2018-01-31 22:39:29 -0600

    Added Procfile to run locally test
    Added Procfile

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
 <?php
-require __DIR__ . "/src/Repository/UserRepository.php";
-$UserRepo = new UserRepository();
-$email="car";
-$password="admin";
-$users=$UserRepo->showUsers();
-var_dump($users);
+// require __DIR__ . "/src/Repository/UserRepository.php";
+// $UserRepo = new UserRepository();
+// $email="car";
+// $password="admin";
+// $users=$UserRepo->showUsers();
+// var_dump($users);
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+//In the same file, add a new handler to query the database:
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
 ?>
-<ul>
-    <?php foreach ($users as $user):?>
-        <li><span> <?php echo $user["email"];?> </span></li>
-    <?php endforeach?>
-</ul>
+
+
